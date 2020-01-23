2c79b50809941e52d45efe07c5ce5ef00c388997
JimmyMcBride <mcbride967@gmail.com>
2020-01-23 13:22:50 -0600

    first commit

---
+++ template/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+import { Wrapper, Card } from 'bushido-strap';
+
+function App() {
+  return (
+    <Wrapper>
+      <Card>
+        <h2>Hello, world!</h2>
+      </Card>
+    </Wrapper>
+  );
+}
+
+export default App;
